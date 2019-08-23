445bc69c98e134ca8d1a3f36c6338466bd030412
toddt67878 <48630393+toddt67878@users.noreply.github.com>
2019-08-22 22:02:03 -0500

    Add files via upload

---
+++ first_repeated_element.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+
+def first_repeated_element(arr)
+  return nil if arr.empty?
+ 
+  counter_hash = Hash.new{0}
+
+  arr.each do |e|
+    counter_hash[e] > 1
+    
+    return e if counter_hash[e] > 1
+  end
+
+  return nil
+end
+
+describe 'first_repeated_element' do
+  it 'returns the first repeated element from an array' do
+    arr_one = [5, 3, 4, 5, 3, 1]
+    expect(first_repeated_element(arr_one)).to eq(5)
+
+    arr_two = [3, 5, 3, 4 5, 3, 1]
+    expect(first_repeated_element(arr_two))to eq(3)
+  end
+
+  it 'returns nil for an empty array' do
+    arr = []
+    expect(first_repeated_element(arr)).to eq(nil)
+  end
+
+  it 'returns nil if no duplicates are found' do
+    arr = [1, 2, 3]
+    expect(first_repeated_element(arr)).to eq(nil)
+  end
+end
